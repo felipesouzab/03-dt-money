bac3b0172d0d020343bf9a4862117bdbcd59dda3
Felipe Souza <felipesouzabarbosa@yahoo.com.br>
2023-07-26 00:17:43 -0300

    first commit

---
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -0,0 +1,68 @@
+import { MagnifyingGlass } from 'phosphor-react'
+import { SearchFormContainer } from './styles'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { TransactionsContext } from '../../../../contexts/TransactionsContext'
+import { useContextSelector } from 'use-context-selector'
+
+/**
+ * Por que que um componente renderiza?
+ * - Hooks changed (mudou estado, contexto, reducer)
+ * - Props changed (mudou propriedades)
+ * - Parent rerendered (componente pai renderizou)
+ *
+ * Qual o fluxo de renderização?
+ * 1. O react recria o HTML da interface daquele componente
+ * 2. Compara a versão do HTML recriada com a versão anterior
+ * 3. SE mudou alguma coisa, ele reescreve o HTML na tela
+ *
+ * Memo:
+ * 0. Hooks changed, Props changed (deep comparison)
+ * 0.1 Comparar com a versão anterior dos hooks e props
+ * 0.2: SE mudou algo, ele vai permitir a nova renderização
+ * 0.3 SE não mudou nada ele nem entra no fluxo de renderização do React
+ */
+
+const searchFormSchema = z.object({
+  query: z.string(),
+})
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>
+
+export function SearchForm() {
+  const fetchTransactions = useContextSelector(
+    TransactionsContext,
+    (context) => {
+      return context.fetchTransactions
+    },
+  )
+
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SearchFormInputs>({
+    resolver: zodResolver(searchFormSchema),
+  })
+
+  async function handleSearchTransactions(data: SearchFormInputs) {
+    await fetchTransactions(data.query)
+  }
+
+  return (
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+      <input
+        type="text"
+        placeholder="Busque por transações"
+        {...register('query')}
+      />
+
+      <button type="submit" disabled={isSubmitting}>
+        <MagnifyingGlass size={20} />
+        Buscar
+      </button>
+    </SearchFormContainer>
+  )
+}
